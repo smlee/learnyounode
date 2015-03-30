8a5dd66fa73da7254d27d1e43e5109e1d8366cbe
smlee <sangmlee23@gmail.com>
2015-03-30 19:21:52 -0400

    cleaned up babySteps

--- babySteps.js
+++ babySteps.js
@@ -1,14 +1,10 @@
 var sum = 0;
 
 function sumArgs() {
-    if (typeof process.argv[2] == 'undefined') {
-        return sum;
-    } else {
-        sum += Number(process.argv.pop());
-        sumArgs();
-    }
-
-    return sum;
+    if (typeof process.argv[2] == 'undefined') return sum;
+    else sum += Number(process.argv.pop());
+    return sumArgs();
 }
-
+// figured everyone will submit it with a for loop
+// started from the end to shorten the line of codes
 console.log(sumArgs());
